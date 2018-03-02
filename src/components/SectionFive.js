751e8510c16e7d1ba51327023c73782fbb9be5fd
Alireza Ghaffari <ghafari.pro@gmail.com>
2018-03-03 00:50:09 +0330

    creating sections

--- src/components/SectionFive.js
+++ src/components/SectionFive.js
@@ -6,6 +6,16 @@ class SectionFive extends React.Component {
   render(){
     return(
       <div className="sections" id="section-five">
+        <div className="sub-section">
+          <div className="footer-container">
+            <p>آرسیس شماره یک ارائه دهنده نرم افزار آنلاین مدیریت زنجیره فروش و صندوق فروشگاهی آنلاین است. آرسیس به شما این امکان را می دهد کسب و کار خود را به آسانی توسعه دهید</p>
+          </div>
+          <div className="footer-container">
+            <h1>لینک ها</h1>
+          </div>
+          <div className="footer-container"></div>
+          <div className="footer-container"></div>
+        </div>
       </div>
     );
   }
